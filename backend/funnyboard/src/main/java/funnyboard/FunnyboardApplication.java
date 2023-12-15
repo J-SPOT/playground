913ec611de408a1d9f3e3b44b32ccc8259f9a336
ji-jjang <jiny_us@daum.net>
2023-12-16 01:22:01 +0900

    [ADD] 스프링 기본 설정

---
+++ backend/funnyboard/src/main/java/funnyboard/FunnyboardApplication.java
@@ -0,0 +1,13 @@
+package funnyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunnyboardApplication.class, args);
+    }
+
+}
