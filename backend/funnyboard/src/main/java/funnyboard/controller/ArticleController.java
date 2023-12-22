1eb2ad7b8b96888fa710b841ab3df471f37deb94
ji-jjang <jinyhehe@gmail.com>
2023-12-22 18:59:10 +0900

    [ADD] 게시글 CRUD api 구현

--- backend/funnyboard/src/main/java/funnyboard/controller/ArticleController.java
+++ backend/funnyboard/src/main/java/funnyboard/controller/ArticleController.java
@@ -1,15 +1,19 @@
 package funnyboard.controller;
 
+import funnyboard.dto.ArticleForm;
 import funnyboard.entity.Article;
 import funnyboard.repository.ArticleRepository;
 import funnyboard.service.ArticleService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @Tag(name = "ArticleController", description = "게시물 CRUD API")
 public class ArticleController {
 
@@ -24,5 +28,38 @@ public class ArticleController {
     @GetMapping("/api/articles")
     public List<Article> findAllArticles() {
         return articleService.findAllArticles();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<Article> findArticleById(@PathVariable Long id) {
+        Article article = articleService.findArticleById(id);
+        return (article != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(article) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
+        Article created = articleService.create(dto);
+
+        return (created != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+        Article updated = articleService.update(id, dto);
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        Article deleted = articleService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.OK).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
